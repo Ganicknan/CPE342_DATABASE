ca1b52f6a2db437236e059c930add1aa7b342e34
Ganicknan <ganicknan.nanjaruwong@gmail.com>
2019-12-04 23:59:18 +0700

    - Create order.html - Path to order.html

--- CPE342_DATABASE/App/urls.py
+++ CPE342_DATABASE/App/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='app-home'),
     path('index.html', views.home, name='app-home'),
-    path('product.html', views.product, name='app-product')
+    path('product.html', views.product, name='app-product'),
+    path('order.html', views.order, name='all-order')
 ]
