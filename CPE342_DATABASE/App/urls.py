a6e2160980b6acb3ea4949fcb7592c2a96e6874c
Ganicknan <ganicknan.nanjaruwong@gmail.com>
2019-12-10 00:17:32 +0700

    create all web page.

--- CPE342_DATABASE/App/urls.py
+++ CPE342_DATABASE/App/urls.py
@@ -9,5 +9,12 @@ urlpatterns = [
     path('order.html', views.order, name='app-order'),
 
     # Employee
-    path('emp_product.html', views.emp_product, name='app-employee-product')
+    path('emp_order.html', views.emp_order, name='app-employee-order'),
+    path('emp_stock-in.html', views.emp_stockIn, name='app-employee-stockIn'),
+    path('emp_customer.html', views.emp_customer, name='app-employee-customer'),
+    path('emp_ERM.html', views.emp_ERM, name='app-employee-ERM'),
+    path('emp_add-coupon.html', views.emp_addCoupon, name='app-employee-addCoupon'),
+    path('order/edit_order.html', views.edit_order, name='app-editOrder'),
+    path('product/edit_stock.html', views.edit_stock, name='app-editStock'),
+    path('customer/edit_customer.html', views.edit_customer, name='app-editCustomer'),
 ]
--- CPE342_DATABASE/App/views.py
+++ CPE342_DATABASE/App/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
-# Create your views here.
+# Customer site.
 def home(request):
     return render(request, 'web/index.html')
 
@@ -14,6 +14,35 @@ def order(request):
     order = Orders.objects.all()
     return render(request, 'web/order.html', {'order': order})
 
-def emp_product(request):
+# Employee site.
+def emp_order(request):
+    order = Orders.objects.all()
+    return render(request, 'web/emp_order.html', {'order': order})
+
+def emp_stockIn(request):
     product = Products.objects.all()
-    return render(request, 'web/emp_product.html', {'product': product})
+    return render(request, 'web/emp_stock-in.html', {'product': product})
+
+def emp_customer(request):
+    customer = Customers.objects.all()
+    return render(request, 'web/emp_customer.html', {'customer': customer})
+    
+def emp_ERM(request):
+    employee = Employees.objects.all()
+    return render(request, 'web/emp_ERM.html', {'employee': employee})
+
+def emp_addCoupon(request):
+    return render(request, 'web/emp_add-coupon.html')
+
+# Edit DATABASE site.
+def edit_order(request):
+    order = Orders.objects.all()
+    return render(request, 'web/edit_order.html', {'order': order})
+    
+def edit_stock(request):
+    product = Products.objects.all()
+    return render(request, 'web/edit_stock.html', {'product': product})
+    
+def edit_customer(request):
+    customer = Customers.objects.all()
+    return render(request, 'web/edit_customer.html', {'customer': customer})
