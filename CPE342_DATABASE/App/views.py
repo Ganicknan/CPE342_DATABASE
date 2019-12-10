a04d5d027f6b4a1feeedaeca958b708cee0a9a73
pumipatsumrit <pumipatsumrit@gmail.com>
2019-12-10 21:05:37 +0700

    update extends-base and decorate

--- CPE342_DATABASE/App/views.py
+++ CPE342_DATABASE/App/views.py
@@ -4,45 +4,45 @@ from .models import *
 
 # Customer site.
 def home(request):
-    return render(request, 'web/index.html')
+    return render(request, 'web/index.html',{'title': 'Home'})
 
 def product(request):
     product = Products.objects.all()
-    return render(request, 'web/product.html', {'product': product})
+    return render(request, 'web/product.html', {'product': product,'title': 'Product'})
 
 def order(request):
     order = Orders.objects.all()
-    return render(request, 'web/order.html', {'order': order})
+    return render(request, 'web/order.html', {'order': order , 'title': 'Order'})
 
 # Employee site.
 def emp_order(request):
     order = Orders.objects.all()
-    return render(request, 'web/emp_order.html', {'order': order})
+    return render(request, 'web/emp_order.html', {'order': order , 'title': 'Emp_order'})
 
 def emp_stockIn(request):
     product = Products.objects.all()
-    return render(request, 'web/emp_stock-in.html', {'product': product})
+    return render(request, 'web/emp_stock-in.html', {'product': product , 'title': 'Emp_stockin'})
 
 def emp_customer(request):
     customer = Customers.objects.all()
-    return render(request, 'web/emp_customer.html', {'customer': customer})
+    return render(request, 'web/emp_customer.html', {'customer': customer , 'title': 'Emp_customer'})
     
 def emp_ERM(request):
     employee = Employees.objects.all()
-    return render(request, 'web/emp_ERM.html', {'employee': employee})
+    return render(request, 'web/emp_ERM.html', {'employee': employee,'title': 'Emp_ERM'})
 
 def emp_addCoupon(request):
-    return render(request, 'web/emp_add-coupon.html')
+    return render(request, 'web/emp_add-coupon.html' , {'title': 'addcoupon'})
 
 # Edit DATABASE site.
 def edit_order(request):
     order = Orders.objects.all()
-    return render(request, 'web/edit_order.html', {'order': order})
+    return render(request, 'web/edit_order.html', {'order': order , 'title': 'edit_order'})
     
 def edit_stock(request):
     product = Products.objects.all()
-    return render(request, 'web/edit_stock.html', {'product': product})
+    return render(request, 'web/edit_stock.html', {'product': product , 'title': 'edit_stock'})
     
 def edit_customer(request):
     customer = Customers.objects.all()
-    return render(request, 'web/edit_customer.html', {'customer': customer})
+    return render(request, 'web/edit_customer.html', {'customer': customer , 'title': 'edit_customer'})
