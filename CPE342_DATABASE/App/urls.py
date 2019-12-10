edc5834262fcc2f5f92405815d77e0673c8adafc
KotoriSN <35366326+KotoriSN@users.noreply.github.com>
2019-12-11 00:25:16 +0700

    update url

--- CPE342_DATABASE/App/urls.py
+++ CPE342_DATABASE/App/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     # Customer
     path('', views.home, name='app-home'),
-    path('index.html', views.home, name='app-home'),
-    path('product.html', views.product, name='app-product'),
-    path('order.html', views.order, name='app-order'),
+    path('index', views.home, name='app-home'),
+    path('product', views.product, name='app-product'),
+    path('order', views.order, name='app-order'),
 
     # Employee
-    path('login.html', views.login, name='app.employee-login'),
-    path('emp_product.html', views.emp_product, name='app-employee-product'),
-    path('emp_ERM.html', views.get_name, name='app-employee-ERM')
+    path('login', views.login, name='app.employee-login'),
+    path('emp_product', views.emp_product, name='app-employee-product'),
+    path('emp_ERM', views.get_name, name='app-employee-ERM')
 ]
